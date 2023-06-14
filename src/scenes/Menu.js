06b62417bb5e92789c29489d4edbd86483548e8e
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2023-06-14 14:37:26 -0300

    imagenes cargadas, menu iniciado

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,6 +4,13 @@ export default class Menu extends Phaser.Scene {
     }
 
     init(){}
-    create(){}
+    create(){
+      this.add.image(400,300, "fondoMenu").setScale(0.24);
+      this.add.image(400, 300, "trenMenu").setScale(0.24);
+      this.add.image(400, 300, "nombreMenu").setScale(0.24);
+      const jugarMenu = this.add.image(400, 300, "jugarMenu1").setScale(0.24).setInteractive();
+      const creditosMenu = this.add.image(400, 300, "creditosMenu1").setScale(0.24).setInteractive();
+      const ayudaMenu = this.add.image(400, 300, "ayudaMenu1").setScale(0.24).setInteractive();
+    }
     upload(){}
 }
--- src/scenes/Precargas.js
+++ src/scenes/Precargas.js
@@ -4,6 +4,20 @@ export default class Precargas extends Phaser.Scene {
     }
 
     init(){}
-    create(){}
+    preload(){
+      this.load.image("fondoMenu", "./public/images/fondoMenu.png");
+      this.load.image("trenMenu", "./public/images/trenMenu.png");
+      this.load.image("nombreMenu", "./public/images/nombreMenu.png");
+      this.load.image("jugarMenu1", "./public/images/jugarMenu1.png");
+      this.load.image("jugarMenu2", "./public/images/jugarMenu2.png");
+      this.load.image("creditosMenu1", "./public/images/creditosMenu1.png");
+      this.load.image("creditosMenu2", "./public/images/creditosMenu2.png");
+      this.load.image("ayudaMenu1", "./public/images/ayudaMenu1.png");
+      this.load.image("ayudaMenu2", "./public/images/ayudaMenu2.png");
+      
+    }
+    create(){
+      this.scene.start("menu");
+    }
     upload(){}
 }
