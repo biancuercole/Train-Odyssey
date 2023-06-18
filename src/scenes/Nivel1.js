8ba6aaa944bac419776e5dc640a671e580493a7a
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2023-06-18 20:26:24 -0300

    se comienza el movimiento de nubes

--- src/scenes/Nivel1.js
+++ src/scenes/Nivel1.js
@@ -1,14 +1,43 @@
 export default class Nivel1 extends Phaser.Scene {
-    constructor() {
-      super("nivel1");
-    }
+  constructor() {
+    super("nivel1");
+  }
 
-    init(){}
+  create() {
+    this.add.image(400, 300, "fondo");
+    this.add.text(400, 300, "nivel1");
 
-    create(){
-      this.add.image(400,300, "fondo");
-      this.add.text(400,300, "nivel1")
-      this.cursors = this.input.keyboard.createCursorKeys();
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.nubesGrupo = this.physics.add.group();
+    this.nubesMoviendo = false; // Variable para controlar si las nubes se están moviendo
+
+    this.input.keyboard.on('keydown_RIGHT', () => {
+      this.nubesMoviendo = true; // Activar el movimiento de las nubes
+      this.addNube();
+    });
+
+    this.input.keyboard.on('keyup_RIGHT', () => {
+      this.nubesMoviendo = false;
+    });
+  }
+
+  update() {}
+
+  addNube() {
+    if (this.nubesMoviendo) {
+      const randomNube = Phaser.Math.RND.pick([
+        "nube1",
+        "nube2",
+        "nube3",
+        "nube4",
+        "nube5",
+        "nube6",
+        "nube7",
+      ]);
+      const randomY = Phaser.Math.RND.between(0, 200);
+      const nube = this.nubesGrupo.create(800, randomY, randomNube);
+      nube.body.setAllowGravity(false); // Desactivar la gravedad en la nube
+      nube.body.setVelocityX(-100); // Establecer la velocidad hacia la izquierda
     }
-    upload(){}
+  }
 }
--- src/scenes/Precargas.js
+++ src/scenes/Precargas.js
@@ -14,10 +14,17 @@ export default class Precargas extends Phaser.Scene {
       this.load.image("creditosMenu2", "./public/images/Menu/creditosMenu2.png");
       this.load.image("ayudaMenu1", "./public/images/Menu/ayudaMenu1.png");
       this.load.image("ayudaMenu2", "./public/images/Menu/ayudaMenu2.png");
-
+      this.load.image("nube1", "./public/images/nubes/nube1.png");
+      this.load.image("nube2", "./public/images/nubes/nube2.png");
+      this.load.image("nube3", "./public/images/nubes/nube3.png");
+      this.load.image("nube4", "./public/images/nubes/nube4.png");
+      this.load.image("nube5", "./public/images/nubes/nube5.png");
+      this.load.image("nube6", "./public/images/nubes/nube6.png");
+      this.load.image("nube7", "./public/images/nubes/nube7.png");
     }
     create(){
       this.scene.start("menu");
+      const nubes = ["nube1", "nube2", "nube3", "nube4", "nube5", "nube6", "nube7"]
     }
     upload(){}
 }
