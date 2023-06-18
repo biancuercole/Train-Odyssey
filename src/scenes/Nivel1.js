657540d44144f023581807a42ca6e6c811f6ee22
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2023-06-18 20:46:43 -0300

    nubes se mueven, arreglar errores

--- src/scenes/Nivel1.js
+++ src/scenes/Nivel1.js
@@ -7,23 +7,44 @@ export default class Nivel1 extends Phaser.Scene {
     this.add.image(400, 300, "fondo");
     this.add.text(400, 300, "nivel1");
 
-    this.cursors = this.input.keyboard.createCursorKeys();
     this.nubesGrupo = this.physics.add.group();
     this.nubesMoviendo = false; // Variable para controlar si las nubes se están moviendo
+    this.nubesTimer = null; // Temporizador para la generación de nubes
 
-    this.input.keyboard.on('keydown_RIGHT', () => {
-      this.nubesMoviendo = true; // Activar el movimiento de las nubes
-      this.addNube();
+    this.input.keyboard.on('keydown', (event) => {
+      if (event.key === 'ArrowRight' && !this.nubesMoviendo) {
+        this.nubesMoviendo = true; // Activar el movimiento de las nubes
+        this.nubesTimer = this.time.addEvent({
+          delay: 3000, // Generar una nueva nube cada 3 segundos (3000 milisegundos)
+          callback: this.generarNubeAleatoria,
+          callbackScope: this,
+          loop: true // Repetir el temporizador indefinidamente
+        });
+      }
     });
 
-    this.input.keyboard.on('keyup_RIGHT', () => {
-      this.nubesMoviendo = false;
+    this.input.keyboard.on('keyup', (event) => {
+      if (event.key === 'ArrowRight') {
+        this.nubesMoviendo = false;
+        if (this.nubesTimer) {
+          this.nubesTimer.destroy(); // Detener el temporizador de generación de nubes
+        }
+        this.nubesGrupo.setVelocityX(0); // Detener el movimiento de las nubes existentes
+      }
     });
   }
 
-  update() {}
+  update() {
+    if (this.nubesMoviendo) {
+      this.nubesGrupo.getChildren().forEach((nube) => {
+        if (nube.x < -nube.width) {
+          nube.destroy(); // Eliminar las nubes que salen de la pantalla
+        }
+      });
+    }
+  }
 
-  addNube() {
+  generarNubeAleatoria() {
     if (this.nubesMoviendo) {
       const randomNube = Phaser.Math.RND.pick([
         "nube1",
