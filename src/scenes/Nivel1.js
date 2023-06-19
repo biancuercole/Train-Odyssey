16b72a0e2337884e01be223a0052327a512e0ccc
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2023-06-19 01:55:38 -0300

    fondo nivel agregado y terminado parcial

--- src/scenes/Nivel1.js
+++ src/scenes/Nivel1.js
@@ -6,6 +6,19 @@ export default class Nivel1 extends Phaser.Scene {
   create() {
     this.add.image(400, 300, "fondo");
     this.add.text(400, 300, "nivel1");
+    const width = 2000;
+    const height = 600;
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    const fondo = this.add.tileSprite(0, 0, width, height, 'nivel1');
+    fondo.setOrigin(0, 0);
+    const velocidadDesplazamiento = 2;
+    this.update = () => {
+      if (this.cursors.right.isDown) {
+      fondo.tilePositionX += velocidadDesplazamiento;
+      }
+    };
+
   }
 
   update() {}
--- src/scenes/Precargas.js
+++ src/scenes/Precargas.js
@@ -14,13 +14,7 @@ export default class Precargas extends Phaser.Scene {
       this.load.image("creditosMenu2", "./public/images/Menu/creditosMenu2.png");
       this.load.image("ayudaMenu1", "./public/images/Menu/ayudaMenu1.png");
       this.load.image("ayudaMenu2", "./public/images/Menu/ayudaMenu2.png");
-      this.load.image("nube1", "./public/images/nubes/nube1.png");
-      this.load.image("nube2", "./public/images/nubes/nube2.png");
-      this.load.image("nube3", "./public/images/nubes/nube3.png");
-      this.load.image("nube4", "./public/images/nubes/nube4.png");
-      this.load.image("nube5", "./public/images/nubes/nube5.png");
-      this.load.image("nube6", "./public/images/nubes/nube6.png");
-      this.load.image("nube7", "./public/images/nubes/nube7.png");
+      this.load.image("nivel1", "./public/images/nivel.png");
     }
     create(){
       this.scene.start("menu");
