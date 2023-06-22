27db679d1726b116161da713ba22703077ded179
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2023-06-22 16:26:05 -0300

    overlap entre pinza y moneda

--- src/scenes/Nivel1.js
+++ src/scenes/Nivel1.js
@@ -6,40 +6,45 @@ export default class Nivel1 extends Phaser.Scene {
   init() {
     this.velocidadBackground = 0.5;
     this.velocidadParallax = 2;
+    this.contadorMonedas = 0
   }
 
   create() {
-    //valores para fondo
+    
+
+    //agregar fondo y parallax
     const width = 2000;
     const height = 600;
-
-    this.cursors = this.input.keyboard.createCursorKeys();
-    //agregar fondo y parallax
     this.fondo = this.add.tileSprite(0, 0, width, height, 'background');
     this.fondo.setOrigin(0, 0);
     this.parallax = this.add.tileSprite(0, 0, width, height, "parallax");
     this.parallax.setOrigin(0, 0);
+
     //agregar interfaz
     this.add.image(1000, 300, "interfaz");
     this.add.image(20, 20, "moneda");
     this.add.image(1300, 300, "vidas");
     this.add.image(1605, 300, "distancia");
 
-    this.grupoMoneda = this.physics.add.group({allowGravity: false});
-    this.pinza = this.physics.add.sprite(950, 295, "pinza");
+    //pinza
+    this.pinza = this.physics.add.sprite(432, 450, "pinza");
     this.pinza.body.allowGravity = false;
-    //agregar sprite de tren y sacar gravedad
-    this.tren = this.physics.add.sprite(950, 300, 'trenSheet');
-    this.tren.body.allowGravity = false;
 
+    //monedas 
+    this.grupoMoneda = this.physics.add.group({allowGravity: false});
     this.time.addEvent ({
-    delay: 15000,
+    delay: 1000,
     callback: this.agregarMoneda, 
     callbackScope: this,
     loop: true,
     });  
+    
+    //agregar sprite de tren y sacar gravedad
+    this.tren = this.physics.add.sprite(950, 300, 'trenSheet');
+    this.tren.body.allowGravity = false;
 
-
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.physics.add.overlap(this.pinza, this.grupoMoneda, this.colectarMoneda, null, this);
   }
 
   update() {
@@ -56,8 +61,8 @@ export default class Nivel1 extends Phaser.Scene {
       this.tren.anims.stop('right');
       this.grupoMoneda.setVelocityX(-0);
     }    
-    const limiteSuperior = 200;
-    const limiteInferior = 295;
+    const limiteSuperior = 320;
+    const limiteInferior = 420;
     const pinzaY = this.pinza.y;
     const isPinzaEnLimiteSuperior = pinzaY <= limiteSuperior;
     const isPinzaEnLimiteInferior = pinzaY >= limiteInferior;
@@ -83,9 +88,16 @@ export default class Nivel1 extends Phaser.Scene {
   }
 
   agregarMoneda() {
-    if(this.cursors.right.isDown) {
-    let moneda = this.grupoMoneda.create(800, 300, "moneda");
-    } 
+    if (this.cursors.right.isDown) {
+      let moneda = this.grupoMoneda.create(800, 260, "moneda");
+      };
+    this.physics.add.overlap(this.pinza, this.moneda, this.colectarMoneda, null, this);
+  }
+
+
+  colectarMoneda(moneda, pinza) {
+    pinza.disableBody(true, true);
+    console.log("Prueba")
   }
 }
 
